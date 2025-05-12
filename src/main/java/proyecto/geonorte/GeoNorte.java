523c818743747106549787181506b3b998b2f66e
Rubén <foompe@gmail.com>
2025-05-12 10:19:05 +0200

    Inicializamos primer repositorio netbeans

---
+++ src/main/java/proyecto/geonorte/GeoNorte.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.geonorte;
+
+import proyecto.geonorte.vista.Menu;
+
+/**
+ *
+ * @author FP DAM
+ */
+public class GeoNorte {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
