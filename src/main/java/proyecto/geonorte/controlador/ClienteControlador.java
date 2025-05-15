79ea786f04cbe609c82f11c4df5de94ed6b24b6f
Rubén <foompe@gmail.com>
2025-05-15 12:24:52 +0200

    Inicializamos primer repositorio netbeans

--- src/main/java/proyecto/geonorte/controlador/ClienteControlador.java
+++ src/main/java/proyecto/geonorte/controlador/ClienteControlador.java
@@ -8,6 +8,7 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import proyecto.geonorte.modelo.dao.ClienteDAO;
 import proyecto.geonorte.vista.ClienteJFrame;
 import proyecto.geonorte.vista.ConsultaClienteJPanel;
 import proyecto.geonorte.vista.ListaClientesJPanel;
@@ -25,29 +26,29 @@ public class ClienteControlador {
     private MenuJFrame menu;
     private ClienteJFrame cliente;
     private ListaClientesJPanel lisCliente;
-    private NuevoClienteJPanel nuevoCliente;
+    private NuevoClienteControlador nuevoCliente;
     private ModificClienteJPanel modificCliente;
     private ConsultaClienteJPanel consultaCliente;
+    private ClienteDAO clienteDAO = new ClienteDAO();
     
     
     
     
-    public ClienteControlador() {
+    public ClienteControlador(MenuJFrame menuVista) {
+        this.menu = menuVista;
         cliente = new ClienteJFrame();
         inicializarEventos();       //inicializamos el metodo encargado de escuchar
        
         cliente.setVisible(true);
         
         lisCliente = new ListaClientesJPanel();
-        mostrarPanelCentral(lisCliente);
-    
-        
+        mostrarPanelCentral(lisCliente);  
     }
     
     
     //metodo para mostrar pantallas en el panel central
     public void mostrarPanelCentral(JPanel panel) {
-        
+        System.out.println("Cambiando panel central a: " + panel.getClass().getSimpleName());
         panel.setSize(1000,600);
         panel.setLocation(0,0);
         
@@ -57,17 +58,65 @@ public class ClienteControlador {
         cliente.getPanelPantalla().repaint();
     }
     
+    
+    public void volverLista() {
+        mostrarPanelCentral(lisCliente);
+    }
+    
     private void inicializarEventos() {
         cliente.getjButtonNuevoCliente().addActionListener(new ActionListener() { 
             public void actionPerformed(ActionEvent e) {
-                
-                mostrarPanelCentral(nuevoCliente);
+                //accion boton nuevo cliente
+                System.out.println("Has pulsado nuevo cliente");
+                nuevoCliente = new NuevoClienteControlador(ClienteControlador.this, clienteDAO);
+                mostrarPanelCentral(nuevoCliente.getVista());
                 
             }
         });
         
+        cliente.getjButtonModificarCliente().addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //accion boton modificar cliente 
+                System.out.println("Has pulsado modificar cliente");
+                modificCliente = new ModificClienteJPanel();
+                mostrarPanelCentral(modificCliente);
+            }
+        });
         
+        cliente.getjButtonConsultarCliente().addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //accion boton consultar cliente
+                System.out.println("Has pulsado consultar cliente");
+                consultaCliente = new ConsultaClienteJPanel();
+                mostrarPanelCentral(consultaCliente);
+                
+            }
+        });
         
+        cliente.getjButtonBorrarCliente().addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //accion boton borrar cliente
+                System.out.println("Has pulsado borrar cliente");
+                
+            }
+        });
         
+        cliente.getjButtonAtras().addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //accion boton atras
+                System.out.println("Has pulsado atras");
+                cliente.dispose();      //cerramos la ventana
+                menu.setVisible(true); //traemos menu de vuelta
+            }
+        });
+        
+        cliente.getjButtonSalir().addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                //accion boton salir
+                System.out.println("Has pulsado Salir");
+                System.exit(0);
+                
+            }
+        });
     }
 }
